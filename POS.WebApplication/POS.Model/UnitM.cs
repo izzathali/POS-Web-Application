013166a87b46575d90b563edcc9cc3fe146daa1d
Izzath Ali <izzath.info@gmail.com>
2022-09-07 00:57:50 +0530

    Unit add part completed

---
+++ POS.WebApplication/POS.Model/UnitM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Model
+{
+    public class UnitM : BaseEntity
+    {
+        [Key]
+        public int UnitId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Unit { get; set; } = string.Empty;
+    }
+}
